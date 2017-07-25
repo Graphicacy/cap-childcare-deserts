a16b9fa6ff5f3fb9628893990acf463bad73cb3c
Ben Southgate <bsouthga@gmail.com>
2017-07-25 18:43:44 -0400

    feat: analyzer

--- dev/webpack.config.ts
+++ dev/webpack.config.ts
@@ -2,6 +2,8 @@ import * as webpack from 'webpack';
 import * as HTMLPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 
+const AnalyzePlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 const templateContent = `
 <!DOCTYPE html>
 <html>
@@ -15,63 +17,70 @@ const templateContent = `
 </html>
 `.trim();
 
-const config: webpack.Configuration = {
-  entry: './src/index.tsx',
-  output: {
-    filename: 'bundles/main.js',
-    path: path.join(__dirname, '../public')
-  },
+export default function(env: any = {}) {
+  const config: webpack.Configuration = {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundles/main.js',
+      path: path.join(__dirname, '../public')
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: 'source-map',
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'source-map',
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-    mainFields: ['module', 'jsnext:main', 'browser', 'main']
-  },
-
-  module: {
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          compilerOptions: {
-            module: 'es2015',
-            target: 'es5'
-          }
-        }
-      },
-
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      mainFields: ['module', 'jsnext:main', 'browser', 'main'],
+      alias: {
+        react: 'preact-compat',
+        'react-dom': 'preact-compat'
       }
+    },
+
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              module: 'es2015',
+              target: 'es5'
+            }
+          }
+        },
+
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+
+    devServer: {
+      contentBase: path.join(__dirname, '../public')
+    },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        __ACCESS_TOKEN__: JSON.stringify(require('../keys.json').accessToken)
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new HTMLPlugin({
+        filename: 'index.html',
+        templateContent
+      }),
+      ...(env.analyze ? [new AnalyzePlugin()] : [])
     ]
-  },
+  };
 
-  devServer: {
-    contentBase: path.join(__dirname, '../public')
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      __ACCESS_TOKEN__: JSON.stringify(require('../keys.json').accessToken)
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new HTMLPlugin({
-      filename: 'index.html',
-      templateContent
-    })
-  ]
-};
-
-export default config;
+  return config;
+}
