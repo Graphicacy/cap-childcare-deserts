f80046316ccd0fc7380c9f3b94c7a0b77f0ef30e
Ben Southgate <bsouthga@gmail.com>
2017-07-31 00:59:26 -0400

    fix: initial state

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,8 +1,6 @@
 import { createElement } from 'react';
 import { connect } from 'react-redux';
 import { Map as ReactMapboxGl, Layer, Feature } from 'react-mapbox-gl';
-import { style } from 'typestyle';
-import { content, height } from 'csstips';
 
 import { StateName } from '../../data';
 import { State } from '../../store';
@@ -11,14 +9,9 @@ declare const __ACCESS_TOKEN__: string;
 
 const MapBoxMap = ReactMapboxGl({ accessToken: __ACCESS_TOKEN__ });
 
-const mapContainerClass = style(content, height(200), {
-  marginTop: -10,
-  backgroundColor: 'green'
-});
-
 const Map = ({ selectedState }: { selectedState: StateName | null }) =>
-  <div className={mapContainerClass}>
-    {/* <MapBoxMap
+  <div>
+    <MapBoxMap
       style="mapbox://styles/bsouthga/cj5ci49b504wq2todg2cjvw7w"
       containerStyle={{
         height: '400px',
@@ -28,7 +21,7 @@ const Map = ({ selectedState }: { selectedState: StateName | null }) =>
       <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
         <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
       </Layer>
-    </MapBoxMap> */}
+    </MapBoxMap>
   </div>;
 
 export default connect((state: State) => ({
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
-import reducer from './reducers';
+import { default as reducer, State } from './reducers';
+import { Action, ActionType, setSelectedState } from './actions';
 import { createStore } from 'redux';
 
-export { Action, ActionType, setSelectedState } from './actions';
-export { State } from './reducers';
+export { State, Action, ActionType, setSelectedState };
 
-export function initStore(initialState = {}) {
+export function initStore(
+  initialState: State = { selectedState: 'All states' }
+) {
   const store = createStore(reducer, initialState);
   return store;
 }
