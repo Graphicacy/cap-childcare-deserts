e3e45a1fef2b82070d8b6b6f062e4beb673c8baf
Ben Southgate <bsouthga@gmail.com>
2017-08-01 21:46:41 -0400

    container

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,32 +8,38 @@ import { Map } from './map/';
 import { State } from '../store/';
 
 const contentClass = style(content);
-const containerClass = style(fillParent, vertical, {
+const contentContainerClass = style(fillParent, vertical, {
   fontFamily: 'Open Sans',
   fontSize: 14
+});
+
+const containerClass = style({
+  width: '100%',
+  height: '100%',
+  overflowX: 'hidden'
 });
 
 type AppProps = Readonly<{
   embed: boolean;
 }>;
 
-const App = (props: AppProps) =>
-  props.embed
-    ? <Map />
-    : <div>
-        <Header />
-        <div className={containerClass}>
-          <div className={contentClass}>
-            <Map />
-            <Banner />
-          </div>
-          <div className={contentClass}>
-            <ChartRow />
-          </div>
-          <div className={contentClass}>
-            <Article />
-          </div>
-        </div>
-      </div>;
+const Full = () =>
+  <div>
+    <Header />
+    <div className={containerClass}>
+      <div className={contentContainerClass}>
+        <Map />
+        <Banner />
+      </div>
+      <div className={contentClass}>
+        <ChartRow />
+      </div>
+      <div className={contentClass}>
+        <Article />
+      </div>
+    </div>
+  </div>;
+
+const App = (props: AppProps) => (props.embed ? <Map /> : <Full />);
 
 export default connect((state: State) => ({ embed: state.embed }))(App);
