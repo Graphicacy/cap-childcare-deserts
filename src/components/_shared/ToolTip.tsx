96ef2cecbbad719ed8f3f60934fc4bfe0d310ec4
Ben Southgate <bsouthga@gmail.com>
2017-08-25 21:07:55 -0400

    fix: center tooltip on mobile

--- src/components/_shared/ToolTip.tsx
+++ src/components/_shared/ToolTip.tsx
@@ -31,10 +31,10 @@ function getToolTipPosition({ state, x, y, embed, mobile }: ToolTipProps) {
   // tract is fixed in corner
   if (state.show === true && state.data.kind === 'tract')
     return {
-      right: embed && mobile ? '50%' : 40,
-      top: embed && mobile ? '50%' : HEADER_HEIGHT + 30,
-      transform: embed && mobile ? 'translate(50%, -50%)' : '',
-      pointerEvents: embed && mobile ? 'all' : 'none'
+      right: mobile ? '50%' : 40,
+      top: mobile ? '50%' : HEADER_HEIGHT + 30,
+      transform: mobile ? 'translate(50%, -50%)' : '',
+      pointerEvents: mobile ? 'all' : 'none'
     };
 
   // otherwise render above mouse
